8d919c0ff661b6ef32e080b3d57ac7d3347a77a9
moscalir <moscalir@uni.coventry.ac.uk>
2020-02-23 20:33:32 +0000

    Set up the token

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,8 +1,11 @@
 const express = require ("express");
 const router = express.Router();
 const gravatar = require("gravatar");
-const bcrypt = require ("bcryptjs")
+const bcrypt = require ("bcryptjs");
+const config = require("config");
 const {check, validationResult} = require("express-validator");
+const jwt = require("jsonwebtoken");
+
 
 const User = require("../../models/User");
 
@@ -27,7 +30,7 @@ router.post("/",[
         let user = await User.findOne({email});
 
         if(user){
-            return res.status(400).json({errors:[{msg:"User already exists"}]});
+            return res.status(400).json({errors:[{msg:"User already exists"}] });
         }
 
     // Get users avatar
@@ -36,7 +39,7 @@ router.post("/",[
             r: "pg",
             d: "mm"
         }))
-
+        // create user
         user = new User({
             name,
             email,
@@ -44,19 +47,28 @@ router.post("/",[
             password,
         });
         
-    // Encrypt password
+    // Encrypt password (hash)
 
         const salt = await bcrypt.genSalt(10);
         user.password = await bcrypt.hash(password, salt);
+
+    // save user in DB    
         await user.save();
 
     // Return jsonwebtoken
-
-        
-        res.send("User registered");
+        const payload = {
+            user:{
+                id: user.id
+            }
+        };
+        jwt.sign(payload, config.get("jwtToken"),{expiresIn: 360000}, 
+        (err, token) =>{
+            if(err) throw err;
+            res.json({ token });
+        });
     } catch (err) {
         console.error(err.message);
-        res.status(500).sent("Server erro")
+        res.status(500).sent("Server error");
     }
 
 
