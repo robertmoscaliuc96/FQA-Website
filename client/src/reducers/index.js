bead1276ec32c8fd39ecebcfbeb4dfc535f6af87
moscalir <moscalir@uni.coventry.ac.uk>
2020-03-16 11:59:12 +0000

    Init post redux

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,8 +1,10 @@
 import {combineReducers} from 'redux';
 import alert from './alert';
 import auth from './auth';
+import auth from './post';
 
 export default combineReducers({
     alert,
-    auth
+    auth,
+    post
 });
