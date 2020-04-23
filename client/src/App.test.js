89c7c03ad20011862aa31f2d84c661531d4b7bde
moscalir <moscalir@uni.coventry.ac.uk>
2020-04-23 18:44:51 +0100

    Testing

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,9 +1,25 @@
 import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import { renderIntoDocument, cleanup } from '@testing-library/react';
+import ReactDOM from "react-dom";
+import Spinner from '../src/components/layout/Spinner';
+import Navbar from '../src/components/layout/Navbar';
+import {shallow} from 'enzyme';
+import Login from './components/auth/Login';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+afterEach(cleanup)
+
+it(" Spinner renders without crashing", () => {
+    const div = document.createElement("div");
+    ReactDOM.render(<Spinner></Spinner>, div)
+})
+
+describe ('Navbar Component', () =>{
+  it(" Navbar renders without errors", () => {
+    const component =shallow(<Navbar></Navbar>);
+    ReactDOM.render(<Spinner></Spinner>, div)
+})
+
+
+})
+
+
