5270f72beebafe374f9af1250a254eba0635f2e9
moscalir <moscalir@uni.coventry.ac.uk>
2020-04-24 13:56:02 +0100

    last commit

---
+++ client/src/acceptance_test/login.spec.js
@@ -0,0 +1,64 @@
+'use strict'
+
+const puppeteer = require('puppeteer')
+const { configureToMatchImageSnapshot } = require('jest-image-snapshot')
+const shell = require('shelljs')
+
+const width = 800
+const height = 600
+const delayMS = 5
+
+let browser
+let page
+let har
+
+// threshold is the difference in pixels before the snapshots dont match
+const toMatchImageSnapshot = configureToMatchImageSnapshot({
+	customDiffConfig: { threshold: 2 },
+	noColors: true,
+})
+expect.extend({ toMatchImageSnapshot })
+
+beforeAll( async() => {
+	browser = await puppeteer.launch({ headless: true, slowMo: delayMS, args: [`--window-size=${width},${height}`] })
+	page = await browser.newPage()
+	har = new PuppeteerHar(page)
+	await page.setViewport({ width, height })
+
+})
+
+afterAll( async() => {
+	browser.close()
+	await shell.exec('acceptanceTests/scripts/afterAll.sh')
+})
+
+beforeEach(async() => {
+	await shell.exec('acceptanceTests/scripts/beforeEach.sh')
+})
+
+describe('Registering', () => {
+	test('Register a user', async done => {
+		//start generating a trace file.
+		await page.tracing.start({path: 'trace/registering_user_har.json',screenshots: true})
+
+
+		await page.goto('http://localhost:3000/login', { timeout: 30000, waitUntil: 'load' })
+		await page.type('input[name=user]', 'test')
+		await page.type('input[name=pass]', 'test')
+		await page.click('input[type=submit]')
+		//ASSERT
+		//check that the user is taken to the homepage after attempting to login as the new user:
+		await page.waitForSelector('h1')
+		expect( await page.evaluate( () => document.querySelector('h1').innerText ) )
+			.toBe('Home')
+
+		// grab a screenshot
+		const image = await page.screenshot()
+		// compare to the screenshot from the previous test run
+		expect(image).toMatchImageSnapshot()
+		// stop logging to the trace files
+		await page.tracing.stop()
+		await har.stop()
+		done()
+	}, 16000)
+})
