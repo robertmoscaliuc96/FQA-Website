4ba4ce4508813762c91916d72b8d4f420804b737
moscalir <moscalir@uni.coventry.ac.uk>
2020-09-03 17:32:08 +0300

    prepare to deploy

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 const express = require("express");
 const connectDB = require("./config/db");
 const path = require('path');
-const nodemailer = require('nodemailer');
+
 
 const app = express();
 
