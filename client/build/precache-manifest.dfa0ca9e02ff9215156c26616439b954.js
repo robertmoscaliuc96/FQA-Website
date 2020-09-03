ae55c2d12b25be94838e23ada70d4a55d3930387
moscalir <moscalir@uni.coventry.ac.uk>
2020-09-04 01:56:30 +0300

    a

---
+++ client/build/precache-manifest.dfa0ca9e02ff9215156c26616439b954.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7e0b74099b2982791c9284dcc1d6b18",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5ffc189d864330151b28",
+    "url": "/static/css/main.a2bf381e.chunk.css"
+  },
+  {
+    "revision": "1b0edf402bbb5f6dfa97",
+    "url": "/static/js/2.e318324b.chunk.js"
+  },
+  {
+    "revision": "603816eec26c1aff4a38bef8d08d40a6",
+    "url": "/static/js/2.e318324b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5ffc189d864330151b28",
+    "url": "/static/js/main.4d5b8ddf.chunk.js"
+  },
+  {
+    "revision": "702554416a81a1e3a894",
+    "url": "/static/js/runtime-main.784c586e.js"
+  },
+  {
+    "revision": "8de631fd071cc1a1ea197a67c590e012",
+    "url": "/static/media/background.8de631fd.jpg"
+  }
+]);
